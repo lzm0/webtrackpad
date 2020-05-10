72eda242ce51ebb99230e010c4bb8dba6512eabc
Zimo Li <7163127+lzm0@users.noreply.github.com>
2020-05-10 13:39:01 +0800

    Add HTTP server

--- webtrackpad.py
+++ webtrackpad.py
@@ -1,8 +1,12 @@
 import asyncio
 import websockets
 from pymouse import PyMouse
+from http import HTTPStatus
 
 mouse = PyMouse()
+f = open("index.html", "rb")
+byte = f.read()
+f.close()
 
 def move(x, y):
     pos = mouse.position()
@@ -19,7 +23,13 @@ async def server(websocket, path):
         else: 
             coordinate = [float(x) for x in message.split(',')]
             move(coordinate[0], coordinate[1])
+
+async def process_request(path, request_headers):
+    if request_headers['Connection'] == 'keep-alive':
+        return (HTTPStatus.OK, [('Content-Type', 'text/html')], byte)
+    else:
+        return None;
             
 asyncio.get_event_loop().run_until_complete(
-    websockets.serve(server, '0.0.0.0', 8080))
+    websockets.serve(server, '0.0.0.0', 80, process_request = process_request))
 asyncio.get_event_loop().run_forever()
